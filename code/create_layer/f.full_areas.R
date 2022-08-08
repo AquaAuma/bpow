3bf83a6840a9c03097f9b3b5bcc5ead8ccc9e855
Aurore Maureaud <46930001+AquaAuma@users.noreply.github.com>
2022-08-08 14:02:15 -0400

    correct problem in northeast US

--- code/create_layer/f.full_areas.R
+++ code/create_layer/f.full_areas.R
@@ -15,6 +15,10 @@ library(exactextractr)
 library(Morpho)
 library(rgl)
 library(units)
+library(rnaturalearth)
+library(rnaturalearthdata)
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
 
 ### load holes transformed into polygons with regions from e. ran on the HPC
 load("outputs/holes/seafloor_meow_deepsea_filled_1_to_5.RData")
@@ -92,3 +96,44 @@ xx <- st_is_valid(bpow) # all true so shapefile valid!
 
 st_write(obj = bpow, dsn = "outputs/bpow/bpow_p8s5_limit_5500.shp")
 
+
+### check and plot all regions
+for(i in 1:100){
+  png(filename = paste0("figures/all_regions/",i,"_",bpow$type[i],".png"),
+  width = 16*200, height = 10*200, res = 200)
+  print(ggplot(world) + geom_sf(fill = "grey", color = NA) +
+    geom_sf(data = bpow[i,], fill = "blue", color = "black") +
+    ggtitle(bpow$prov_n[i]))
+  dev.off()
+}
+
+### problem with 20 and 24
+new_1 <- bpow[20,] %>% 
+  st_cast(geometry, to = "POLYGON")
+pb_1 <- bpow[20,] %>% 
+  st_cast(geometry, to = "POLYGON") %>% 
+  st_centroid() %>% 
+  st_coordinates()
+new_1 <- cbind(new_1, pb_1) %>% 
+  filter(!(X<0 & X>(-90))) %>% 
+  dplyr::select(-X, -Y) %>% 
+  group_by(type, type_id, prov_n, prov_id, source, ID) %>% 
+  summarize(geometry = st_union(geometry)) %>% 
+  ungroup()
+st_geometry(bpow[20,]) <- st_geometry(new_1)
+
+new_2 <- bpow[24,] %>% 
+  st_cast(geometry, to = "POLYGON")
+pb_2 <- bpow[24,] %>% 
+  st_cast(geometry, to = "POLYGON") %>% 
+  st_centroid() %>% 
+  st_coordinates()
+new_2 <- cbind(new_2, pb_2) %>% 
+  filter(Y<0) %>% 
+  dplyr::select(-X, -Y) %>% 
+  group_by(type, type_id, prov_n, prov_id, source, ID) %>% 
+  summarize(geometry = st_union(geometry)) %>% 
+  ungroup()
+st_geometry(bpow[24,]) <- st_geometry(new_2)
+
+st_write(obj = bpow, dsn = "outputs/bpow/bpow_p8s5_limit_5500.shp")
