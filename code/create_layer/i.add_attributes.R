8eb5e458550f59fe51a3d0e6b47666f281129ab2
Aurore Maureaud <aurore.aqua@gmail.com>
2023-03-14 14:51:01 -0400

    add depth range description in layer

--- code/create_layer/i.add_attributes.R
+++ code/create_layer/i.add_attributes.R
@@ -6,7 +6,11 @@ library(ggplot2)
 
 bpow <- st_read(dsn = "/Users/auroremaureaud/Documents/bpow_p10.shp") 
 attributes <- st_read("outputs/bpow/bpow_p8s5_abyssal_corr.shp") %>% 
-  st_drop_geometry()
+  st_drop_geometry() %>% 
+  mutate(depth_r = ifelse(type = "coastal", "0-800m", NA_character_),
+         depth_r = ifelse(type = "bathyal", "800-3,500m", depth_r),
+         depth_r = ifelse(type == "abyssal", "3,500-6,500m", depth_r),
+         depth_r = ifelse(type == "hadal", "6,500m+", depth_r))
 
 bpow <- left_join(bpow, attributes, by = "ID")
 
